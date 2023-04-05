4bb45d893121980a5b4f5768abadaa04272ca4ee
Anna Sidiakina <fruktik@gmail.com>
2023-04-05 21:23:32 +0200

    delete team

--- src/pages/api/team/index.js
+++ src/pages/api/team/index.js
@@ -1,30 +1,5 @@
-// import { getTeam, addTeam } from "@component/lib/mongodb/team";
-
-// const handler = async (req, res) => {
-//   if (req.method === "GET") {
-//     try {
-//       const { team, error } = await getTeam();
-//       if (error) throw new Error(error);
-//       return res.status(200).json({ team });
-//     } catch (e) {
-//       return res.status(500).json({ e: e.message });
-//     }
-//   }
-//   if (req.method === "POST") {
-//     try {
-//       await addTeam(req.body);
-//       return res.status(200).json("A team member is successfully added.");
-//     } catch (e) {
-//       return res.status(500).json({ e: e.message });
-//     }
-//   }
-//   res.setHeader("Allow", ["GET"]);
-//   res.status(425).end(`Method ${req.method} is not allowed.`);
-// };
-// export default handler;
-
 import dbConnect from "@component/lib/mongodb/dbConnect";
-import TeamMember from "@component/models/teamMember";
+import TeamMember from "@component/models/TeamMember";
 
 export default async function handler(req, res) {
   const { method } = req;
